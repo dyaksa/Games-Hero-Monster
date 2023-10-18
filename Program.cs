ba2447097a0bdfde62c2f0ffb6d7b7800f248b41
dyaksa <diasnour0395@gmail.com>
2023-10-18 16:29:33 +0700

    monster hero damage

--- Program.cs
+++ Program.cs
@@ -1,40 +1,18 @@
-﻿string message = "The quick brown fox jumps over the lazy dog.";
-bool result = message.Contains("dog");
-Console.WriteLine(result);
-
-if (message.Contains("fox")) 
-{
-    Console.WriteLine("What dies the fox say?");
-}
+﻿int hero = 10;
+int monster = 10;
 
 Random dice = new Random();
-int roll1 = dice.Next(1,7);
-int roll2 = dice.Next(1,7);
-int roll3 = dice.Next(1,7);
 
-var total = roll1 + roll2 + roll3;
+do {
+    int roll = dice.Next(1,11);
+    monster -= roll;
+    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
 
-Console.WriteLine($"Dice : {roll1} + {roll2} + {roll3}");
-Console.WriteLine(total);
+    if(monster <= 0) continue;
 
-if((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
-{
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total +=2;
-}
+    roll = dice.Next(1,11);
+    hero -= roll;
+    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+}while(hero > 0 && monster > 0);
 
-if ((roll1 == roll2) && (roll2 == roll3))
-{
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
-}
-
-if(total >= 15) 
-{
-    Console.WriteLine("You Win!");
-}
-
-if(total < 15){
-    Console.WriteLine("Sorry, you lose!");
-}
-
+Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
